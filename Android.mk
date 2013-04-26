7ec9ca41995d7ea4b3e9e30b4e244d9d71f013fe
Naiem Shaik <snaiem@codeaurora.org>
2013-04-25 18:35:26 -0700

    VeNum optimizations to libpng to improve PNG decode time
    Set correct counter in neon routine for SUB filter type.
    Enable Neon optimizations for all filter types and pixel depths.
    cherry picked from commit b912f64bc4bb174fc055cda58e303faaa640b8b1
    
    Change-Id: I4ff1c78637db1ab91689ad90da9b5e7da07d2a9e

--- Android.mk
+++ Android.mk
@@ -74,6 +74,11 @@ include $(BUILD_STATIC_LIBRARY)
 # For the device (shared)
 # =====================================================
 
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+   common_SRC_FILES += contrib/pngneon/png_read_filter_row_neon.s
+   common_CFLAGS += -D__ARM_HAVE_NEON
+endif
+
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
